515a51c812d71e7d11461c186215bd57ce7abbae
Sergei Nikitin <42813631+sanikitin@users.noreply.github.com>
2021-02-24 22:40:59 +0300

    minor refactoring

--- Boss Level 1 - The Dicee Game/index.js
+++ Boss Level 1 - The Dicee Game/index.js
@@ -1,22 +1,18 @@
-function randomDice1() {
-    let n1 = Math.random();
-    n1 = Math.floor(n1*6) + 1;
-    return n1;
+function randomDice() {
+    let n = Math.random();
+    n = Math.floor(n * 6) + 1;
+    return n;
 }
-let d1 = randomDice1();
-document.getElementById("dice1").setAttribute("src", "images/dice"+d1+".png");
 
-function randomDice2() {
-    let n2 = Math.random();
-    n2 = Math.floor(n2*6) + 1;
-    return n2;
-}
-let d2 = randomDice2();
-document.getElementById("dice2").setAttribute("src", "images/dice"+d2+".png");
+let d1 = randomDice();
+document.getElementById("dice1").setAttribute("src", "images/dice" + d1 + ".png");
 
-if (d1>d2) {
+let d2 = randomDice();
+document.getElementById("dice2").setAttribute("src", "images/dice" + d2 + ".png");
+
+if (d1 > d2) {
     document.getElementById("result").textContent = "🚩Player 1 Win!";
-} else if (d1<d2) {
+} else if (d1 < d2) {
     document.getElementById("result").textContent = "Player 2 Win!🚩"
 } else {
     document.getElementById("result").textContent = "DRAW!"
