87410884a45945306754f3e95ea25d579d68d7cf
Sergei Nikitin <42813631+sanikitin@users.noreply.github.com>
2021-03-01 17:18:52 +0300

    switchcase and this

--- 9. Advanced Javascript/Drum Kit/index.js
+++ 9. Advanced Javascript/Drum Kit/index.js
@@ -1,3 +1,42 @@
-document.querySelector("button").addEventListener("click", function () {
-    alert ("I got clicked!")
-});
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        let buttonThis = this.innerHTML;
+
+        switch (buttonThis) {
+            case "w":
+                let tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                let tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "s":
+                let tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                let tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                let crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            case "k":
+                let kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            case "l":
+                let snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+
+            default: console.log(buttonThis);
+                break;
+        }
+    });
+}
+
