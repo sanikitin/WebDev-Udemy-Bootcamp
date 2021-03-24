d93fb563e0e5c86a46aae01f25a44565229d7f3a
Sergei Nikitin <42813631+sanikitin@users.noreply.github.com>
2021-03-24 22:32:06 +0300

    add success and failure page

--- 13. API/NewsLetter-SingUp/app.js
+++ 13. API/NewsLetter-SingUp/app.js
@@ -47,10 +47,20 @@ app.post("/", function (req, res) {
         response.on("data", function (data) {
             console.log(JSON.parse(data));
         })
+
+        if (response.statusCode===200) {
+            res.sendFile(__dirname+"/success.html");
+        } else {
+            res.sendFile(__dirname+"/failure.html");
+        }
     })
 
     request.write(jsonData);
     request.end();
+});
+
+app.post("/failure", function (req, res) {
+    res.redirect("/");
 });
 
 app.listen(port, function (port) {
