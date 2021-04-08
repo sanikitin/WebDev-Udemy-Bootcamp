4fc9ddaaf6bdf93de67f6346a470ce644135e86b
Sergei Nikitin <42813631+sanikitin@users.noreply.github.com>
2021-04-08 19:59:28 +0300

    add lodash and have been done tasks 16,17,18

--- 15. Boss Level 3 - Blog Website/app.js
+++ 15. Boss Level 3 - Blog Website/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { forEach } = require("lodash");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,6 +19,20 @@ let posts = [];
 
 app.get("/", function (req,res) {
   res.render("home", {homeStartingContent: homeStartingContent, posts: posts});  
+});
+
+app.get("/posts/:postName", function (req, res) {
+  const postName = _.lowerCase(req.params.postName);
+
+  posts.forEach(post => { 
+    const title = _.lowerCase(post.postTitle);
+
+    if (title === postName) {
+      console.log("Match found!");
+    } else {
+      console.log("Match not found!");
+    }
+  });
 });
 
 app.get("/contact", function (req,res) {
