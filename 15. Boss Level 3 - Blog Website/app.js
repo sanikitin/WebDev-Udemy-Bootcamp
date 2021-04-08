1e060e0db3e31a3b53febe75831e84f0a73da1fb
Sergei Nikitin <42813631+sanikitin@users.noreply.github.com>
2021-04-08 15:00:13 +0300

    add first five tasks

--- 15. Boss Level 3 - Blog Website/app.js
+++ 15. Boss Level 3 - Blog Website/app.js
@@ -13,18 +13,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function (req,res) {
+  res.render("home", {homeStartingContent: homeStartingContent});  
+});
 
+app.get("/contact", function (req,res) {
+  res.render("contact", {contactContent: contactContent});  
+});
 
-
-
-
-
-
-
-
-
-
-
+app.get("/about", function (req,res) {
+  res.render("about", {aboutContent: aboutContent});  
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
